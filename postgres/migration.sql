6ae9ef58b793be014deed659adb6a2295e0fc05e
Calmantara <calmantarasp@gmail.com>
2023-03-26 09:44:11 +0700

    feat(final): refactor docker file

---
+++ postgres/migration.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE "user_management";
+GRANT ALL PRIVILEGES ON DATABASE "user_management" TO postgres;
+
+CREATE DATABASE "photo_management";
+GRANT ALL PRIVILEGES ON DATABASE "user_management" TO postgres;
