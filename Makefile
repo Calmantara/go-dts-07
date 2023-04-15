2627b2c14812485aa5a1ec6569b0bb59ef026ad1
Calmantara <calmantarasp@gmail.com>
2023-04-15 10:28:20 +0700

    fix docker compose

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ build:
 	make -C ./postgres build-image
 	make -C ./nginx build-image
 	make -C ./app/go-dts-user build-image
-	make -C ./redis build-image
+	# make -C ./redis build-image
 up:
 	docker compose --compatibility -f docker/compose.yaml up --build -d
 up-stack:
--- app/go-dts-user/deployment/script/build.sh
+++ app/go-dts-user/deployment/script/build.sh
@@ -2,14 +2,14 @@
 
 program="go-dts-user"
 
-# ensure all is up to date
-go mod tidy
+# # ensure all is up to date
+# go mod tidy
 
-# make artifact for program 
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o $program
+# # make artifact for program 
+# CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o $program
 
 # make docker
 docker build --no-cache	 -t $program -f deployment/build/Dockerfile . 
 
 # clean artifact
-rm -rf $program
+# rm -rf $program
